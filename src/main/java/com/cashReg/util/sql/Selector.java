4c4b22903150ec6e1e91d91c2859847dbc34b4a5
Maksym Zelinskyi <maximzelinskyi@gmail>
2024-05-20 09:47:37 +0200

    Columns are now addressed by their names and not indexes

--- src/main/java/com/cashReg/util/sql/Selector.java
+++ src/main/java/com/cashReg/util/sql/Selector.java
@@ -1,7 +1,6 @@
 package com.cashReg.util.sql;
 
 import com.cashReg.Warehouse;
-import com.cashReg.controllers.OrderController;
 import com.cashReg.util.dao.DataSource;
 import com.cashReg.models.Order;
 import com.cashReg.models.OrderItem;
@@ -10,7 +9,6 @@ import com.cashReg.models.User;
 import com.cashReg.util.SQLList;
 import com.cashReg.util.SQLMap;
 import com.cashReg.util.UserUtil;
-import org.apache.log4j.Logger;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,8 +16,6 @@ import java.time.LocalDate;
 import java.util.*;
 
 public class Selector extends AbstractExecutor {
-
-    private static final Logger log = Logger.getLogger(Selector.class);
 
     static {
         try {
@@ -34,10 +30,10 @@ public class Selector extends AbstractExecutor {
         try(ResultSet resultSet = executeSelect(SELECT_ALL + "\"user\"")) {
             List<User> result = new ArrayList<>();
             while (resultSet.next()) {
-                long id = resultSet.getLong(1);
-                int roleId = resultSet.getInt(2);
-                String username = resultSet.getString(3);
-                String password = resultSet.getString(4);
+                long id = resultSet.getLong("id");
+                int roleId = resultSet.getInt("role_id");
+                String username = resultSet.getString("username");
+                String password = resultSet.getString("password");
 
                 User user = UserUtil.createUser(username, password, roleId);
                 user.setId(id);
@@ -54,10 +50,10 @@ public class Selector extends AbstractExecutor {
         Map<Product, Long> result = new HashMap<>();
         try(ResultSet resultSet = executeSelect(SELECT_ALL+"\"product\"")) {
             while (resultSet.next()) {
-                long id = resultSet.getLong(1);
-                String name = resultSet.getString(2);
-                float price = resultSet.getFloat(3);
-                long quantity = resultSet.getLong(4);
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                float price = resultSet.getFloat("price");
+                long quantity = resultSet.getLong("quantity");
                 result.put(new Product(id, name, price), quantity);
             }
             return new SQLMap<>(result);
@@ -68,16 +64,14 @@ public class Selector extends AbstractExecutor {
 
     public List<Order> getAllOrders(){
         Warehouse warehouse = Warehouse.getInstance();
-        try(ResultSet resultSet = executeSelect("select o.id, o.customer_id, o.amount, o.customer_email, o.cashier_id, o.date from \"order\" o")){
+        try(ResultSet resultSet = executeSelect(SELECT_ALL+"\"order\"")){
             List<Order> result = new ArrayList<>();
             while(resultSet.next()){
-                long orderId = resultSet.getLong(1);
-                float amount = resultSet.getFloat(3);
-                String customerEmail = resultSet.getString(4);
-                long cashierId = resultSet.getLong(5);
-//                log.info("order date: " +resultSet.getString(5));
-//                log.info("test: " + resultSet.getLong(1));
-                LocalDate date = (LocalDate) (resultSet.getObject(6, LocalDate.class));
+                long orderId = resultSet.getLong("id");
+                float amount = resultSet.getFloat("amount");
+                String customerEmail = resultSet.getString("customer_email");
+                long cashierId = resultSet.getLong("cashier_id");
+                LocalDate date = (LocalDate) (resultSet.getObject("date", LocalDate.class));
 
                 Map<OrderItem, Long> products = new HashMap<>();
                 Order order = new Order();
@@ -91,9 +85,9 @@ public class Selector extends AbstractExecutor {
                         SELECT_ALL+"\"order_item\" WHERE order_id = " + orderId
                 );
                 while(orderItems.next()){
-                    long itemId = orderItems.getLong(1);
-                    long productId = orderItems.getLong(3);
-                    long quantity = orderItems.getLong(4);
+                    long itemId = orderItems.getLong("id");
+                    long productId = orderItems.getLong("product_id");
+                    long quantity = orderItems.getLong("quantity");
                     Product product = Warehouse.getInstance().getProduct(productId);
                     OrderItem item = new OrderItem(product, orderId);
                     item.setId(itemId);
